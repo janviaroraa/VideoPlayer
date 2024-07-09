cb917928a54400d84fbbf01e14f3a131bf03b7d2
janvi-aroraa <jjanvi.arora18@gmail.com>
2024-07-09 12:12:17 +0530

    orientation fix

--- NetflixPlayer/PlayerViewController.swift
+++ NetflixPlayer/PlayerViewController.swift
@@ -16,12 +16,34 @@ class PlayerViewController: UIViewController {
     private var player: AVPlayer?
     private var playerLayer: AVPlayerLayer?
 
+    private var windowOrientation: UIInterfaceOrientation? {
+        return self.view.window?.windowScene?.interfaceOrientation
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .black
     }
 
     override func viewDidAppear(_ animated: Bool) {
         setVideoPlayer()
+    }
+
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: any UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+        guard let windowOrientation else { return }
+
+        if #available(iOS 16.0, *) {
+            if windowOrientation.isPortrait {
+                playerHeight.constant = 300
+            } else {
+                playerHeight.constant = self.view.layer.bounds.height
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                self.playerLayer?.frame = self.videoPlayerView.bounds
+            }
+        }
+
     }
 
     private func setVideoPlayer() {
